ba1b2ce830b60b95561aafa819c8ceca70f24942
Jeremyrrsmith <jeremyrrsmith@gmail.com>
2019-05-14 11:25:07 +0100

    ADD: Create tasks db.
    Added details to travis file to auto create the table and have created the table.

--- db/migrate/20190514095910_create_tasks.rb
+++ db/migrate/20190514095910_create_tasks.rb
@@ -1,7 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-
+      t.string :title
+      t.text :description
       t.timestamps
     end
   end
