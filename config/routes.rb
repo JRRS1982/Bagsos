fb4c6509f66d573b40cca73821395d8d96780e35
Arthur Ashman <arthurashman@gmail.com>
2019-05-16 16:52:54 +0100

    UPDATE: Devise to handle two user types
    Removed user table and replaced with beneficiary and volunteer devise functionality
    respectively.
    Added checks for user type to certain pages in order to restrict certain content
    per user type

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :beneficiaries, path: 'beneficiaries', controllers: {
+    sessions: 'beneficiaries/sessions'
+  }
+  devise_for :volunteers, path: 'volunteers', controllers: {
+    sessions: 'volunteers/sessions'
+  }
+
   get 'welcome/index'
-  devise_for :users
   resources :tasks
   authenticated :user do
     root 'tasks#index', as: :authenticated_root
