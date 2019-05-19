fa0950b2f837222eecf8d9de9c737f4db14f764b
henrygambles <gambles.henry2@gmail.com>
2019-05-19 09:20:52 +0100

    DELETE extra instance of address.string in db migration files - causing Travis to error.

--- db/migrate/20190517135007_add_lat_and_long_float_to_tasks.rb
+++ db/migrate/20190517135007_add_lat_and_long_float_to_tasks.rb
@@ -3,7 +3,6 @@
 class AddLatAndLongFloatToTasks < ActiveRecord::Migration[5.2]
   def change
     change_table :tasks do |t|
-      t.string :address
       t.float :latitude
       t.float :longitude
     end
