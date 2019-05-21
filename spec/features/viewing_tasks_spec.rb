cebd3695064dfd4322f50b9bf02097876e4de5c9
Arthur Ashman <arthurashman@gmail.com>
2019-05-21 20:50:37 +0100

    ADD: testing
    Altered if statement for routing post signup/login so it did not
    need testing
    Addded tests to cover final lines of untested code
    Added a couple more tests
    
    Maps on localhost tasks/show not rendering! WARNING

--- spec/features/viewing_tasks_spec.rb
+++ spec/features/viewing_tasks_spec.rb
@@ -18,13 +18,25 @@ feature 'Viewing tasks' do
   scenario 'volunteers can see a listed task' do
     sign_up_beneficiary
     click_link 'New task'
-    fill_in 'task_title', with: "Help ooooh i need somebody!"
-    fill_in 'task_description', with: "Oooooo i need somebody to loveeeeeeeeeeeeeee, not just anybody!"
+    fill_in 'task_title', with: "Task title"
+    fill_in 'task_description', with: "Task description"
     fill_in 'task_address', with: "50 Commercial Street, London"
     click_button 'List Task'
-    expect(page).to have_content('Task successfully listed')
-    click_link 'Log out'
+    log_out
     sign_up_volunteer
-    expect(page).to have_content("Help Ooooh I Need Somebody!")
+    expect(page).to have_content("Task Title")
+  end
+
+  scenario 'volunteers can open a listed task to see description' do
+    sign_up_beneficiary
+    click_link 'New task'
+    fill_in 'task_title', with: "Task title"
+    fill_in 'task_description', with: "Task description"
+    fill_in 'task_address', with: "50 Commercial Street, London"
+    click_button 'List Task'
+    log_out
+    sign_up_volunteer
+    click_link 'Show Task'
+    expect(page).to have_content("Task description")
   end
 end
