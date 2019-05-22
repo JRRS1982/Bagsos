4f299ae3b9b43da046f4c4b5fdcd5f2b28e81095
henrygambles <gambles.henry2@gmail.com>
2019-05-22 15:39:59 +0100

    UPDATE tasks table - latitude & longitude now lat & lng. Also updated master.key to fix API.

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_22_082831) do
+ActiveRecord::Schema.define(version: 2019_05_22_142848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,10 +20,11 @@ ActiveRecord::Schema.define(version: 2019_05_22_082831) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float "latitude"
-    t.float "longitude"
+    t.float "lat"
+    t.float "lng"
     t.string "address"
     t.bigint "user_id"
+    t.string "type", limit: 30
     t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
@@ -44,5 +43,4 @@ ActiveRecord::Schema.define(version: 2019_05_22_082831) do
   end
 
   add_foreign_key "tasks", "users"
-
 end
