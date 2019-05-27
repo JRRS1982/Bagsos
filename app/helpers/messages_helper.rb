494b9cebc108e231aa063551587cb05fb624d2f9
arthurashman <45827968+arthurashman@users.noreply.github.com>
2019-05-27 12:17:33 +0100

    Update messages_helper.rb

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -2,7 +2,7 @@ module MessagesHelper
   def recipients_options(chosen_recipient = nil)
     s = ''
     User.all.each do |user|
-      s << "<option value='#{user.id}' data-img-src='{gravatar_image_url(user.email, size: 50, alt: 'avatar')}' 
+      s << "<option value='#{user.id}' data-img-src='{gravatar_image_url(user.email, size: 50)}' 
         #{'selected' if user == chosen_recipient}>#{user.name}</option>"
     end
     s.html_safe
