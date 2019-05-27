c127d074c7860ce1a2b5fc7bc9e53a6a852bfa4e
arthurashman <45827968+arthurashman@users.noreply.github.com>
2019-05-27 12:17:05 +0100

    Update application_helper.rb

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,6 @@
 
 module ApplicationHelper
   def gravatar_for(user, size = 30, title = user.name)
-  image_tag gravatar_image_url(user.email, size: size), title: title, class: 'img-rounded', alt: 'profile-pic'
+  image_tag gravatar_image_url(user.email, size: size), title: title, class: 'img-rounded'
   end
 end
